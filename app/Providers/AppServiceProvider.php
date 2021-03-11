328c20673d211f6a3c1fbc019e84306ec3434aa0
Ruprecht <rostevWork@mail.ru>
2021-03-11 11:54:17 +1000

    Setting up the development environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
     }
 
     /**
@@ -25,4 +28,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+
 }
